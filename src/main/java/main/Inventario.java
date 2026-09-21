653998998ac76e864d940bcc92b24689e4606fb2
Victory949 <aaron.victory@ucr.ac.cr>
2026-09-21 02:18:51 -0600

    Creaçión clase Inventario y metodos vacios

---
+++ src/main/java/main/Inventario.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author aaron
+ */
+public class Inventario 
+{
+    //Definir arreglo
+    private Producto[] p = new Producto[10];
+    //Acumulable
+    private int cant = 1;
+    
+    public void registrarProducto()
+    {
+        
+    }//fin metodo registrarProducto
+    
+    public void mostrarProductos()
+    {
+        
+    }//fin metodo mostrarProductos
+    
+    public void buscarProducto()
+    {
+        
+    } //fin metodo buscarProducto
+    
+    public void venderUnidades()
+    {
+        
+    }//fin metodo venderUnidades
+    
+    public void reabastecerProducto()
+    {
+        
+    }//fin metodo reabastecerProducto
+    
+    public void calcTotal()
+    {
+        
+    }//fin metodo calcTotal
+    
+}//fin clase Inventario
