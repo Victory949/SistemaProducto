7d567893019d7c181d91b283cf3d16977a9e76eb
Victory949 <aaron.victory@ucr.ac.cr>
2026-09-21 02:33:31 -0600

    Finalización metodo registrarProducto

--- src/main/java/main/Inventario.java
+++ src/main/java/main/Inventario.java
@@ -4,6 +4,8 @@
  */
 package main;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author aaron
@@ -15,9 +17,54 @@ public class Inventario
     //Acumulable
     private int cant = 1;
     
+    //---------------METODOS---------------
+    //Metodo para solicitar todos los datos
     public void registrarProducto()
     {
-        
+       for(int i=0; i<p.length;i++)
+       {
+           int codigo = Integer.parseInt(JOptionPane.showInputDialog("----Producto #" + cant + "-------"
+                   + "\nIngrese el codigo del producto"));
+
+           String nombre = JOptionPane.showInputDialog("----Producto #" + cant + "-------"
+                   + "\nIngrese el nombre del producto");
+           
+           int cantP = Integer.parseInt(JOptionPane.showInputDialog("----Producto #" + cant + "-------"
+                   + "\nIngrese las existencias disponibles del producto"));
+           
+           double precio = Double.parseDouble(JOptionPane.showInputDialog("----Producto #" + cant + "-------"
+                   + "\nIngrese el precio del producto"));
+           
+           //Comprobación de codigos
+           boolean codigoRepetido = false;
+
+           for (int k = 0; k < p.length; k++) 
+           {
+               if (p[k].getCodigo() == codigo) 
+               {
+                   codigoRepetido = true;
+               }
+           }//fin  ciclo para detectar códigos repetidos
+           
+           
+           //Resultados
+           if (codigo >= 0 && precio > 0 && cantP > 0) 
+           {
+               JOptionPane.showMessageDialog(null, "¡¡¡No repita códigos!!!");
+               i--;
+           } 
+           else if (codigoRepetido == true) 
+           {
+               p[i] = new Producto(codigo, cantP, nombre, precio);
+               cant++;
+           } 
+           else 
+           {
+               JOptionPane.showMessageDialog(null, "Ingrese correctamente los datos");
+               i--;
+           }
+        }//fin ciclo para
+       
     }//fin metodo registrarProducto
     
     public void mostrarProductos()
